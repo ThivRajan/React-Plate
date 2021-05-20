ed2dcc8a7ab6b431e5bbf01ebbec4f342616a66a
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2021-05-19 20:15:50 -0400

    Add explanation comment

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import UserService from './services/userService';
 
 const App = (): JSX.Element => {
 
+	// Example of call to firestore
 	useEffect(() => {
 		getUsers();
 	});
-
 	const getUsers = async () => {
 		const users = await UserService.getAll();
 		console.log('Users:', users);
